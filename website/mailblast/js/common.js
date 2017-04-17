29328615f4976b9c021792f64687562327a0718b
caiyanhu <caiyanhu752925240@qq.com>
2017-04-17 10:47:55 +0800

    fix:修复mailblast主页中第一部分高度问题及字体 Close:#1,#2

--- website/mailblast/js/common.js
+++ website/mailblast/js/common.js
@@ -5,10 +5,10 @@ $(function() {
   //当滚动时导航条变色
   $(document).scroll(function() {
     if ($(document).scrollTop() >= 1) {
-      $('.navbar').addClass('navbar-fixed-top');
+      $('.mailblast nav').css({'position': 'fixed', 'background-color': '#313131', 'z-index': '1100'});
     }
     else {
-      $('.navbar').removeClass('navbar-fixed-top');
+      $('.mailblast nav').css({'position': 'relative', 'background-color': '#32A7B8'});
     }
   });
   $(document).on('click', '.envelope', function() {
